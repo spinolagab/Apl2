acd3ea08e63212bd0cc67b8d01fc1582d888e368
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2024-10-28 14:57:35 -0300

    Estruturação do Projeto

---
+++ src/AVLTree/AVLNode.java
@@ -0,0 +1,29 @@
+package AVLTree;
+
+import BTree.BNode;
+
+public class AVLNode<T> extends BNode<T> {
+   
+    private int fb;
+    public AVLNode(T data, BNode parent) {
+        super(data, parent);
+        fb = 0;
+    }
+
+    public AVLNode(T data) {
+        super(data);
+        fb = 0;
+    }
+
+    public int getFb() {
+        return fb;
+    }
+
+    public void setFb(int fb) {
+        this.fb = fb;
+    }
+    
+    
+    
+   
+}
