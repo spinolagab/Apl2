dea4c6a83e436cd9f0519b3a87e1d9db7d894730
L. Sarai <133360110+Lsarai23@users.noreply.github.com>
2024-11-19 09:05:16 -0300

    fix: remove BST e testes de BST

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,25 +1,21 @@
 package Main;
 
-import AVLTree.AVL;
+import BTree.BST;
 
 public class Main {
     public static void main(String[] args) {
-        AVL avl = new AVL();
-        avl.insertBalanced(10);
-        System.out.println(avl.getRoot());
-        avl.insertBalanced(20);
-        System.out.println(avl.getRoot());
-        avl.insertBalanced(15);
-        System.out.println(avl.getRoot());
-        avl.insertBalanced(17);
-        System.out.println(avl.getRoot());
-        avl.insertBalanced(18);
-        System.out.println(avl.getRoot());
-        avl.insertBalanced(21);
-        System.out.println(avl.getRoot());
-        System.out.println("Antes de remover: " + avl.inOrderTraversal());
-        avl.deleteBalanced(21);
-        avl.deleteBalanced(20);
-        System.out.println("Depois: " + avl.inOrderTraversal());
+        BST bst = new BST();
+        bst.insert(10);
+        bst.insert(20);
+        bst.insert(15);
+        bst.insert(17);
+        bst.insert(18);
+        bst.insert(21);
+        System.out.println("Antes de remover: " + bst.inOrderTraversal());
+        bst.remove(21);
+        System.out.println(bst.getRoot());
+        bst.remove(20);
+        System.out.println(bst.getRoot());
+        System.out.println("Depois: " + bst.inOrderTraversal());
     }
 }
