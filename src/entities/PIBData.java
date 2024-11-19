abe6b0fdd60fd408cbc4f40a8422c42f6cfdb0da
L. Sarai <sarai.gg.lucas@gmail.com>
2024-11-19 15:01:25 -0300

    feat: Getters e Setters de PIBData

--- src/entities/PIBData.java
+++ src/entities/PIBData.java
@@ -12,6 +12,30 @@ public class PIBData {
         this.agropecuaria = agropecuaria;
     }
 
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public Double getAno() {
+        return ano;
+    }
+
+    public void setAno(Double ano) {
+        this.ano = ano;
+    }
+
+    public Double getAgropecuaria() {
+        return agropecuaria;
+    }
+
+    public void setAgropecuaria(Double agropecuaria) {
+        this.agropecuaria = agropecuaria;
+    }
+
     @Override
     public String toString() {
         return "PIBData [mes=" + mes + ", ano=" + ano + ", agropecuaria=" + agropecuaria + "]";
